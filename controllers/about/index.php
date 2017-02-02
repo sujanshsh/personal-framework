bf767e6c18cf17dbeb65468a434d0fa860774078
Sujan <iamsujanshakya@gmail.com>
2017-02-02 22:35:10 +0545

    added css files, use it

--- controllers/about/index.php
+++ controllers/about/index.php
@@ -1,6 +1,6 @@
 <?php
 
-$css_link_tags = '<link href="/css/general.css" rel="stylesheet" />';
+$css_link_tags = '<link href="'.BASE_PATH.'/css/general.css" rel="stylesheet" />';
 $header = ViewParser::parse('../views/common/header.php',array('link_tags_css'=>$css_link_tags));
 $footer = ViewParser::parse('../views/common/footer.php',array());
 
