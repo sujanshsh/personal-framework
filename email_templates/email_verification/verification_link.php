74fc8e48075285297e4ced437940febef7aff306
Sujan <iamsujanshakya@gmail.com>
2017-02-08 21:55:09 +0545

    email message templates

---
+++ email_templates/email_verification/verification_link.php
@@ -0,0 +1,7 @@
+<h4>Email Verification</h4>
+<hr />
+<p style="font-family: sans-serif;">Dear <?=$user_name?>,<br />
+Please Open this <a href="<?=$link?>" target="_blank" style="font-weight: bold;">link</a> to verify your email account.<br />
+Regards,<br />
+Organization Name
+</p>
